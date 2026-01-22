bea1c2979de9a3d5a655c946798cd54c0bf88cf4
Manu04Hubb <emanuelodhiambo84@gmail.com>
2026-01-22 16:08:20 +0300

    local variables

---
+++ main/src/com/manu/java/learning/projects/Test.java
@@ -0,0 +1,15 @@
+package com.manu.java.learning.projects;
+
+import java.util.Scanner;
+
+public class Test {
+    public void pupAge(){
+        int age = -1;
+        age = age + 7;
+        System.out.println("Age is : " + age);
+    }
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.pupAge();
+    }
+}
