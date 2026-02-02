76f5a688a7ba766aff60a38c758d44ee21caa0db
Manu04Hubb <emanuelodhiambo84@gmail.com>
2026-02-02 14:56:44 +0300

    if elseif else statements

--- main/src/com/manu/java/learning/projects/DecisionMakingStatements.java
+++ main/src/com/manu/java/learning/projects/DecisionMakingStatements.java
@@ -1,13 +1,27 @@
 package com.manu.java.learning.projects;
 
+import java.util.Scanner;
+
 public class DecisionMakingStatements {
     public static void main(String[] args){
 //        if else statement
-        int num1 = 30;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your number");
+        int num1 = sc.nextInt();
         if(num1 < 20){
             System.out.println("This is if statement");
         }else{
             System.out.println("This is else");
+        }
+//        else if
+        if (num1 >= 30){
+            System.out.println("The value of num1 is " + num1);
+        }else if(num1 == 10){
+            System.out.println("The value of num1 is " + num1);
+        }else if(num1 == 40){
+            System.out.println("The value of num1 is " + num1);
+        }else{
+            System.out.println("This is an else");
         }
     }
 }
