ead5a8e0aa62d7984793f4fccc22ed6111664d0c
Manu04Hubb <emanuelodhiambo84@gmail.com>
2026-02-04 09:53:11 +0300

    nested ifs

--- main/src/com/manu/java/learning/projects/EmployeeLoanCheck.java
+++ main/src/com/manu/java/learning/projects/EmployeeLoanCheck.java
@@ -1,10 +1,9 @@
 package com.manu.java.learning.projects;
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class EmployeeLoanCheck {
-    public static void main() {
+    public static void main(String[] args) {
         String loanType = "";
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your age : ");
@@ -30,29 +29,32 @@ public class EmployeeLoanCheck {
         System.out.println("Do you have an existing loan (yes or no) : ");
         String hasExistingLoan = sc.next();
 
-        if (age >= 20 && age <= 60 && hasExistingLoan == "yes") {
-            if (employmentType == "permanent" && salary >= 50000) {
-                if (creditScore <= 700 && hasExistingLoan != "no") {
-                    loanType = "standard";
-                    System.out.println("Your employment type is " + employmentType + " loan type " + loanType);
-                } else if(creditScore >= 700 && hasExistingLoan != "yes"){
+        if (age >= 20 && age <= 60 && hasExistingLoan.equals("no")) {
+            if (employmentType.equals("permanent") && salary >= 50000) {
+                if (creditScore >= 700) {
                     loanType = "premium";
-                    System.out.println("Your employment type is " + employmentType + " loan type " + loanType);
-                }else{
-                    System.out.println("YOu are not eligible for the permanent employees loan");
-                }
-
-            } else if (employmentType == "contract" && salary >= 70000) {
-                if (experience >= 2 && creditScore >= 720) {
-                    System.out.println("Welcome you are a " + employmentType + " on " + salary + "a month" + " therefore you are eligible for contract employess loan and you are " + age + " years old");
                 } else {
-                    System.out.println("You are not eligible for contract a loan. ");
+                    loanType = "standard";
+                }
+                    System.out.println("Your loan is approved : " + loanType + " at " + salary + " a month " + " for employment type " + employmentType);
+
+            } else if (employmentType.equals("contract") && salary >= 70000) {
+                if (experience >= 2 && creditScore >= 720) {
+                    loanType = "premium";
+                    System.out.println("Loan approved " + loanType + " for employment type " + employmentType);
+                } else {
+                    System.out.println("Contract loan rejected due to low credit score or experience");
                 }
             }else{
-                System.out.println("You are not eligible for any of the loans. ");
+                System.out.println("You do not meet salary requirements. ");
             }
         }else{
-            System.out.println("Too old for a loan at " + age + " years" + " how will you pay back");
+            if(age > 60){
+                System.out.println("You are too old for a loan at " + age + " years");
+            }
+            else{
+                System.out.println("You already has an existing loan. ");
+            }
         }
 }
 }
